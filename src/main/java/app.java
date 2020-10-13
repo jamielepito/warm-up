874e7352326b06e3be6fe3b1bdd00d5cdc786ef3
James Churchill <james@smashdev.com>
2020-10-13 13:05:58 -0700

    Update App class

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -2,5 +2,8 @@ public class app {
     public static void main(String[] args) {
         System.out.println("Hi Jamie!");
         System.out.println("Straight up hacking");
+
+        System.out.println("Why did the Java Developer quit his job?");
+        System.out.println("Because he didn't get arrays.");
     }
 }
